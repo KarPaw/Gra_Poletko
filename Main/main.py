187c5103b0885113002a8ee7797a9668cd1cac87
KarPaw <k.pawelczyk4@student.uw.edu.pl>
2022-01-24 22:34:42 +0100

    Pierwszy commit z podziałem na klasy. Zapisanie pierwszych celów projektowych oraz pomysłu projektu w plikach tekstowych.

---
+++ Main/main.py
@@ -0,0 +1,9 @@
+import Poletko
+import Rośliny
+import Sklep
+import Szkodniki
+
+
+if __name__ == "__main__":
+    # tutaj sprawdzamy wszystko
+    print("Hello")
---
+++ Rośliny/roślina.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class Roślina(ABC):
+
+    def __init__(self, nazwa, nasiono_cena, skup_cena, czas_wegetacji, woda_cena, szkodniki):
+        self.__nazwa = nazwa
+        self.__nasiono_cena = nasiono_cena
+        self.__skup_cena = skup_cena
+        self.__czas_wegetacji = czas_wegetacji # ile dni rośnie, aż będzie gotowa do zbioru
+        self.__woda_cena = woda_cena
+        self.__szkodniki = szkodniki
